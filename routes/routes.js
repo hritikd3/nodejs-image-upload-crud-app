6f14b862fb356c180bde8c23ea1de77ea3828e87
hritikd3 <hritikdangi3@gmail.com>
2023-07-01 14:34:53 +0530

    server connection maintained

--- routes/routes.js
+++ routes/routes.js
@@ -2,13 +2,16 @@ const express= require('express')
 const router= express.Router();
 
 router.get("/",(req,res)=>{
-    res.send("Welcome to home page ")
+    res.render("index", { 
+      title: "Home Page"        
+    });
 })
-router.get("/users",(req,res)=>{
-    res.send("Welcome to user page ")
+router.get("/add",(req,res)=>{        
+    //parameter as :  file name and title pe jo show karna vo name 
+    res.render("add_users", {title :    "User Page"})
 })
-router.get("/contact",(req,res)=>{
-    res.send("Welcome to contact page ")
-})
+// router.get("/contact",(req,res)=>{
+//     res.send("Welcome to contact page ")
+// })
 
-module.exports= router;
+module.exports= router; 
