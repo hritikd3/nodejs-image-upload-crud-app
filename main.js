64c39ea7a7823ea4d9d137eb7c9e00d24d190896
hritikd3 <hritikdangi3@gmail.com>
2023-07-01 17:45:00 +0530

    updated : session management

--- main.js
+++ main.js
@@ -1,48 +1,47 @@
-//import
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
-
 const app = express();
 const PORT = process.env.PORT || 5500;
 
-mongoose.connect(process.env.DB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-//middleware
-app.use(express.urlencoded({ extended: false }));
+// Middleware
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(
   session({
     secret: "my secret key",
-    saveUninitialized: true,
     resave: false,
+    saveUninitialized: true,
   })
 );
-app.use((req, res, next) => {
-  res.locals.message = req.session.message;
-  delete req.session.message;
-  next();
-});
-//set template engine
+
+// Database connection
+mongoose
+  .connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    
+  })
+  .then(() => console.log("Connected to the database"))
+  .catch((error) => console.error(error));
+
+// Set template engine
 app.set("view engine", "ejs");
 
-//route prefix
-app.use("", require("./routes/routes"));
+// Routes
+const routes = require("./routes/routes");
+app.use("/", routes);
 
-// //database connection
-const db = mongoose.connection;
-db.on("error", (error) => console.log(error));
-db.once("open", () => console.log("connnected to the database"));
-
-// app.get("/", (req, res) => {
-//   res.send("server is running");
-// });
-
-app.listen(PORT, () => {
-  console.log(`app is listening on localhost://${PORT}`);
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Internal Server Error");
 });
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`App is listening on http://localhost:${PORT}`);
+});
+   
